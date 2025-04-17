57c85ed41eaa8550ad9910e54ba4eaf14f5722f3
Jeff <reutherjeff@gmail.com>
2025-04-17 11:31:55 -0400

    updated ts files to fix errors in pathfinder

--- pathfinder/pathfinder.ts
+++ pathfinder/pathfinder.ts
@@ -1,4 +1,4 @@
-const getLocation = (item: string): number | undefined => {
+const getLocation = (item: string): number[] | undefined => {
   for (let i = 0; i < itemList.length; i++) {
     // Starts at the first string.
     for (let j = 2; j < itemList[i].length; j++) {
@@ -6,7 +6,7 @@ const getLocation = (item: string): number | undefined => {
       if (itemList[i][j] === item) {
         const location = itemList[i][1];
         if (Array.isArray(location) && typeof location[0] === 'number') {
-          return location[0];
+          return location;
         }
       }
     }
@@ -21,7 +21,7 @@ const neighbors = (node: number[]): number[][] => {
     const calculation = [node[0] + dir[0], node[1] + dir[1]];
     // If the neighboring node is within the bounds of the gridArray.
     if (calculation[0] >=0 && calculation[1] >= 0 && calculation[0] < 9 && calculation[1] < 56) {
-        // If the neighboring node is not a wall.
+      // If the neighboring node is not a wall.
       if (gridArray[calculation[0]][calculation[1]] === 0) {
           result.push(calculation);
       }
@@ -95,6 +95,246 @@ function singlePath(start: number[], goal: number[]) {
   return path;
 }
 
+function multiPath(items: string[]): void {
+  if (items.length === 0) {
+      return;
+  }
+
+  // Gathers up each start and goal points.
+  const locationNodes: number[][] = [];
+  let fullPath: number[][] = [];
+  for (const item of items) {
+      // Returns an array node for the item string.
+      const location = getLocation(item);
+      if (location === undefined) {
+          console.error(`Item "${item}" not found in itemList.`);
+          continue;
+      }
+      // Pushes the location node to the locationNodes array.
+      locationNodes.push(location);
+  }
+  locationNodes.sort((a, b) => a[2] - b[2]);
+
+  for (let i = 0; i < locationNodes.length; i++) {
+      locationNodes[i].pop();
+  }
+
+  // Used for keeping track of the item locations to visit (gold tile).
+  const destinationNodes = locationNodes.slice();
+  localStorage.setItem("destination-nodes", JSON.stringify(destinationNodes));
+
+  locationNodes.unshift([8, 49]);
+  locationNodes.push([8, 49]);
+  for (let j = 1; j < locationNodes.length; j++) {
+      const result = singlePath(locationNodes[j - 1], locationNodes[j]);
+      fullPath = [...fullPath, ...result];
+  }
+  localStorage.setItem("path", JSON.stringify(fullPath));
+  updateDOM(fullPath, destinationNodes);
+}
+
+function checkIfRepeat(repeatPath: string[], path: number[][], index: number): boolean {
+  for (let i = 0; i < repeatPath.length; i++) {
+      if (repeatPath[i] === path[index] + "") {
+          return true;
+      }
+  }
+  return false;
+}
+
+function renderCanvas(): void {
+  let i = 0;
+  canvas.width = gridArray[i].length * 20;
+  canvas.height = gridArray.length * 20;
+
+  for (let j = 0; j < gridArray[i].length; j++) {
+      if (gridArray[i][j] === 0) {
+          context.fillRect(20 * j, 20 * i, 20, 20);
+          context.clearRect(20 * j + 1, 20 * i + 1, 18, 18);
+      } else {
+          context.fillRect(20 * j, 20 * i, 20, 20);
+      }
+
+      // If it's the end of the current row.
+      if (j + 1 === gridArray[i].length) {
+          i++;
+
+          // If it's the end of the entire gridArray.
+          if (i === gridArray.length) {
+              break;
+          } else {
+              j = -1;
+          }
+      }
+  }
+}
+
+function renderTodoList(): void {
+  if (JSON.parse(localStorage.getItem("itemsToDisplay")!)) {
+      const itemsToDisplay: string[] = JSON.parse(localStorage.getItem("itemsToDisplay")!);
+
+      for (let i = 0; i < itemsToDisplay.length; i++) {
+          addTodo(itemsToDisplay[i]);
+      }
+  }
+}
+
+function addTodo(storedTodo: undefined | string): void {
+  let newTodoText: string, itemsToDisplay: string[];
+  let includes = false;
+
+  // If it's not just refreshing the todo list.
+  if (!storedTodo) {
+      newTodoText = (input as HTMLInputElement).value.trim();
+  } else {
+      newTodoText = storedTodo;
+  }
+
+  // If it's not just refreshing the todo list.
+  if (!storedTodo) {
+      // If this isn't the first todo item, set up itemsToDisplay.
+      if (JSON.parse(localStorage.getItem("itemsToDisplay")!)) {
+          itemsToDisplay = JSON.parse(localStorage.getItem("itemsToDisplay")!);
+          // If newTodoText is not in itemsToDisplay, the rest of the code runs.
+          for (let i = 0; i < itemsToDisplay.length; i++) {
+              if (itemsToDisplay[i] === newTodoText) {
+                  includes = true;
+              }
+          }
+          if (!includes) {
+              // Grab the old value for the itemsToDisplay localStorage object and push the newTodoText.
+              itemsToDisplay.push(newTodoText);
+              // Then reset the itemsToDisplay object key.
+              localStorage.setItem("itemsToDisplay", JSON.stringify(itemsToDisplay));
+          }
+      // If this is the first todo item, set up itemsToDisplay.
+      } else {
+          localStorage.setItem("itemsToDisplay", JSON.stringify([newTodoText]));
+          itemsToDisplay = JSON.parse(localStorage.getItem("itemsToDisplay")!);
+      }
+  }
+
+  if (!includes) {
+      const todo = document.createElement("li");
+      todo.setAttribute("class", "todo-item");
+      todo.setAttribute("data-completed", "false");
+  
+      const text = document.createElement("span");
+      text.setAttribute("class", "todo-text");
+      text.appendChild(document.createTextNode(newTodoText));
+  
+      const toggleButton = document.createElement("input");
+      toggleButton.setAttribute("type", "checkbox");
+      toggleButton.setAttribute("class", "todo-toggle-button");
+      toggleButton.addEventListener("click", function() { toggleTodo(todo) });
+  
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.setAttribute("class", "todo-delete-button");
+      deleteButton.addEventListener("click", function() {
+          itemsToDisplay = JSON.parse(localStorage.getItem("itemsToDisplay")!);
+          itemsToDisplay.splice(itemsToDisplay.indexOf(newTodoText), 1);
+          localStorage.setItem("itemsToDisplay", JSON.stringify(itemsToDisplay));
+          deleteTodo(todo, itemsToDisplay);
+      });
+  
+      if (todoList) {
+        todoList.appendChild(todo);
+      }
+      todo.appendChild(toggleButton);
+      todo.appendChild(text);
+      todo.appendChild(deleteButton);
+  }
+}
+
+function deleteTodo(todo: HTMLElement, itemsToDisplay: string[]): void {
+  if (todoList) {
+      todoList.removeChild(todo);
+  }
+  if (itemsToDisplay.length === 0) {
+      localStorage.removeItem("itemsToDisplay");
+  }
+}
+
+function toggleTodo(todo: HTMLElement): void {
+  const isCompleted = todo.getAttribute("data-completed") === "true";
+  todo.setAttribute("data-completed", (!isCompleted).toString());
+  if (!isCompleted) {
+      (todo.getElementsByClassName("todo-text")[0] as HTMLElement).style.textDecoration = "line-through";
+  } else {
+      (todo.getElementsByClassName("todo-text")[0] as HTMLElement).style.textDecoration = "none";
+  }
+}
+
+function updateDOM(path: number[][], destinationNodes: number[][]): void {
+  // Deep copy of the item list.
+  const itemListCopy = JSON.parse(JSON.stringify(itemList));
+  let repeatPath = JSON.parse(localStorage.getItem("repeat-path")!);
+  let repeat = false;
+  startTime = Date.now();
+  for (let k = 0; k < path.length; k++) {
+      const timeoutId = setTimeout(function(index) {
+          return function() {
+              // If the path reaches one of the destination tiles.
+              if (path[index] + "" === destinationNodes[0] + "") {
+                  let itemName;
+
+                  // Find the name of the destination item.
+                  for (let l = 0; l < itemListCopy.length; l++) {
+                      const removedValue = itemListCopy[l][1].pop();
+                      if (itemListCopy[l][1] + "" === destinationNodes[0] + "") {
+                          itemName = itemListCopy[l][2];
+                          break;
+                      } else {
+                          itemListCopy[l][1].push(removedValue);
+                      }
+                  }
+
+                  // Update the todo-list DOM to indicate the item is found.
+                  for (let m = 0; m < document.getElementsByClassName("todo-text").length; m++) {
+                      if (itemName === document.getElementsByClassName("todo-text")[m].innerHTML) {
+                          (document.getElementsByClassName("todo-text")[m] as HTMLElement).style.textDecoration = "line-through";
+                          let parentElement = document.getElementsByClassName("todo-text")[m].parentElement;
+                          if (parentElement) {
+                              parentElement.setAttribute("data-completed", "true");
+                              (parentElement.children[0] as HTMLInputElement).checked = true;
+                          }
+                      }
+                  }
+
+                  // Update the canvas DOM color to gold to indicate the item is found.
+                  context.fillStyle = "gold";
+                  destinationNodes.shift();
+                  localStorage.setItem("destination-nodes", JSON.stringify(destinationNodes));
+              } else {
+                  context.fillStyle = "red";
+              }
+
+              // If a path "walks" over a node that isn't a destinationNode more than once, it will alternate red and partial red.
+              if (context.fillStyle === "red" || context.fillStyle === "#ff0000") {
+                  repeat = checkIfRepeat(repeatPath, path, index);
+                  if (repeat === true) {
+                      context.fillRect(20 * path[index][1] + 1, 20 * path[index][0] + 1, 18, 18);
+                      context.clearRect(20 * path[index][1] + 1, 20 * path[index][0] + 1, 9, 18);
+                      // Removes repeated node from repeatPath.
+                      repeatPath.splice(repeatPath.indexOf(path[index] + ""), 1);
+                      localStorage.setItem("repeat-path", JSON.stringify(repeatPath));
+                      repeat = false;
+                  } else {
+                      context.fillRect(20 * path[index][1] + 1, 20 * path[index][0] + 1, 18, 18);
+                      // Adds repeated node to repeatPath.
+                      repeatPath.push(path[index] + "");
+                      localStorage.setItem("repeat-path", JSON.stringify(repeatPath));
+                  }
+              } else {
+                  context.fillRect(20 * path[index][1] + 1, 20 * path[index][0] + 1, 18, 18);
+              }
+          };
+      }(k), 250 * k);
+      timeouts.push(timeoutId);
+  }
+}
+
 // Note:  Entrance starts at [8, 49].
 var gridArray = [
   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -141,19 +381,91 @@ var itemList = [
 ];
 
 // DOM references.
-var input = document.getElementById("newItemSelect");
-var canvas = document.getElementById("pathfinder-canvas");
-var context = canvas.getContext("2d");  // Context used in render function.
-var searchButton = document.getElementById("searchButton");
-var refreshButton = document.getElementById("refreshButton");
-var pauseButton = document.getElementById("pauseButton");
-var timeouts = [];
-var startTime, elapsedTime, remainingPaths;
+var input: HTMLElement = document.getElementById("newItemSelect")!;
+var canvas = document.getElementById("pathfinder-canvas") as HTMLCanvasElement;
+var context = (canvas as HTMLCanvasElement).getContext("2d")!;
+var searchButton = document.getElementById("searchButton") as HTMLButtonElement;
+var refreshButton = document.getElementById("refreshButton") as HTMLButtonElement;
+var pauseButton = document.getElementById("pauseButton") as HTMLButtonElement;
+var timeouts: (number | undefined)[] = [];
+var startTime: number, elapsedTime: number, remainingPaths: number[][];
+// Pause button.
 var paused = false;
 // Todo list.
-var todoList = document.getElementById("todo-list");
-var addButton = document.getElementById("addTodo");
+var todoList = document.getElementById("todo-list")!;
+var addButton = document.getElementById("addTodo")!;
 
 addButton.addEventListener("click", function() {
-  addTodo();
+  addTodo(undefined);
 });
+
+// DOM buttons.
+searchButton.addEventListener("click", function(event) {
+  for (let i = 0; i < timeouts.length; i++) {
+      clearTimeout(timeouts[i]);
+  }
+  timeouts = [];
+  paused = false;
+
+  localStorage.setItem("repeat-path", '[]');
+  if (event.target && (event.target as HTMLElement).id === "searchButton") {
+      if (JSON.parse(localStorage.getItem("itemsToDisplay")!) !== null) {
+          renderCanvas();
+          multiPath(JSON.parse(localStorage.getItem("itemsToDisplay")!));
+      }
+  }
+});
+
+refreshButton.addEventListener("click", function() {
+  for (let i = 0; i < todoList.children.length; ) {
+      todoList.removeChild(todoList.children[i]);
+  }
+  for (let i = 0; i < timeouts.length; i++) {
+      clearTimeout(timeouts[i]);
+  }
+  timeouts = [];
+  localStorage.setItem("path", "[]");
+  localStorage.setItem("destination-nodes", "[]");
+  localStorage.setItem("repeat-path", "[]");
+  renderTodoList();
+  renderCanvas();
+})
+
+pauseButton.addEventListener("click", function() {
+  // Clear all timeouts.
+  if (paused === false) {
+      paused = true;
+      for (let i = 0; i < timeouts.length; i++) {
+          clearTimeout(timeouts[i]);
+      }
+      timeouts = [];
+
+      // Removes the paths already traversed from the localStorage path.
+      remainingPaths = JSON.parse(localStorage.getItem("path")!);
+      elapsedTime = Date.now() - startTime;
+      while (elapsedTime >= 250) {
+          remainingPaths.shift();
+          elapsedTime -= 250;
+      }
+      // Removes the current "paused at" node.
+      remainingPaths.shift();
+      localStorage.setItem("path", JSON.stringify(remainingPaths));
+  } else {
+      if (paused === true) {
+          // Clear all timeouts.
+          paused = false;
+          for (let j = 0; j < timeouts.length; j++) {
+              clearTimeout(timeouts[j]);
+          }
+          timeouts = [];
+
+          // Updates DOM without the paths already traversed.
+          if (remainingPaths.length > 0) {
+              updateDOM(remainingPaths, JSON.parse(localStorage.getItem("destination-nodes")!));
+          }
+      }
+  }
+});
+
+renderTodoList();
+renderCanvas();
